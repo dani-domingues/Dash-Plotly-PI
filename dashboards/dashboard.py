e9ea34dcc68481767109f5ba19bdd48071eac309
C1324523 Daniela Domingues Queiros de Oliveira <c1324523@interno.bb.com.br>
2023-05-19 23:46:37 -0300

    ajustes

--- dashboards/dashboard.py
+++ dashboards/dashboard.py
@@ -225,7 +225,7 @@ app.layout = html.Div([
                 children='Para otimizar a fluidez e melhorar a mobilidade no transporte', className="body-do-painel-texto"),
             html.Div(
                 children='''
-                Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de espécimes de tipos. Ele sobreviveu não apenas a cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Foi popularizado na década de 1960 com o lançamento de folhas Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker, incluindo versões de Lorem Ipsum.
+                Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de espécimes de tipos. Ele sobreviveu não apenas a cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. 
                 ''', className="body-do-painel-texto"),
         ]
     ),
