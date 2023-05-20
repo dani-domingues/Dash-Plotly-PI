76d4dda71f6c2948056b7a89c03d19d6ece75831
C1324523 Daniela Domingues Queiros de Oliveira <c1324523@interno.bb.com.br>
2023-05-19 23:16:06 -0300

    Cor dos graficos

--- dashboards/dashboard.py
+++ dashboards/dashboard.py
@@ -29,8 +29,39 @@ fig1.update_layout(
     title='Comparação de Congestionamento por Mês e Região',
     xaxis_title='Mês',
     yaxis_title='Congestionamento (em metros)',
-    barmode='group'
-)
+    barmode='group',
+    plot_bgcolor='rgba(0, 0, 0, 0)',  # Define a cor de fundo do gráfico como transparente
+    paper_bgcolor='#252a48',  # Define a cor de fundo do papel como '#252a48'
+    legend=dict(
+        x=1.02,
+        y=0.98,
+        bgcolor='rgba(255, 255, 255, 0.1)',
+        bordercolor='#252a48',
+        borderwidth=2,
+        font=dict(
+            color='#fff' # Define a cor do texto da legenda como branco
+        )
+    ),
+        title_font=dict(
+        color='#fff'  # Define a cor do título do gráfico como branco
+    ),
+    xaxis=dict(
+        tickfont=dict(
+            color='#fff'  # Define a cor dos números e meses no eixo X como branco
+        ),
+        title_font=dict(
+            color='#fff'  # Define a cor do título do eixo X como branco
+        )
+    ),
+    yaxis=dict(
+        tickfont=dict(
+            color='#fff'  # Define a cor dos números no eixo Y como branco
+        ),
+        title_font=dict(
+            color='#fff'  # Define a cor do título do eixo Y como branco
+        )
+    )
+    )
 
 dias_semana_ordem = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo']
 
@@ -51,7 +82,38 @@ fig2.update_layout(
     title='Comparação de Tamanho por Dia da Semana e Região',
     xaxis_title='Dia da Semana',
     yaxis_title='Congestionamento (em metros)',
-    barmode='group'
+    barmode='group',
+    plot_bgcolor='rgba(0, 0, 0, 0)',  # Define a cor de fundo do gráfico como transparente
+    paper_bgcolor='#252a48',  # Define a cor de fundo do papel como '#252a48'
+    legend=dict(
+        x=1.02,
+        y=0.98,
+        bgcolor='rgba(255, 255, 255, 0.1)',
+        bordercolor='#252a48',
+        borderwidth=2,
+        font=dict(
+            color='#fff' # Define a cor do texto da legenda como branco
+        )
+    ),
+        title_font=dict(
+        color='#fff'  # Define a cor do título do gráfico como branco
+    ),
+    xaxis=dict(
+        tickfont=dict(
+            color='#fff'  # Define a cor dos números e meses no eixo X como branco
+        ),
+        title_font=dict(
+            color='#fff'  # Define a cor do título do eixo X como branco
+        )
+    ),
+    yaxis=dict(
+        tickfont=dict(
+            color='#fff'  # Define a cor dos números no eixo Y como branco
+        ),
+        title_font=dict(
+            color='#fff'  # Define a cor do título do eixo Y como branco
+        )
+    )
 )
 
 
@@ -59,7 +121,42 @@ fig2.update_layout(
 df_agrupado_mes_regiao = df_2022.groupby(['Mes', 'Regiao'], sort=False)['Tamanho'].mean().reset_index()
 df_grouped.head()
 fig3 = px.line(df_agrupado_mes_regiao, x="Mes", y="Tamanho", color="Regiao")
-fig3.update_traces(textposition="bottom right")
+fig3.update_traces(
+    textposition="bottom right"
+)
+
+fig3.update_layout(
+    plot_bgcolor='rgba(0, 0, 0, 0)',  # Define a cor de fundo do gráfico como transparente
+    paper_bgcolor='#252a48',  # Define a cor de fundo do papel como '#252a48'
+    legend=dict(
+        bgcolor='rgba(255, 255, 255, 0.1)',
+        bordercolor='#252a48',
+        borderwidth=2,
+        font=dict(
+            color='white'  # Define a cor do texto da legenda como branco
+        )
+    ),
+    title_font=dict(
+        color='white'  # Define a cor do título do gráfico como branco
+    ),
+    xaxis=dict(
+        tickfont=dict(
+            color='white'  # Define a cor dos números e meses no eixo X como branco
+        ),
+        title_font=dict(
+            color='white'  # Define a cor do título do eixo X como branco
+        )
+    ),
+    yaxis=dict(
+        tickfont=dict(
+            color='white'  # Define a cor dos números no eixo Y como branco
+        ),
+        title_font=dict(
+            color='white'  # Define a cor do título do eixo Y como branco
+        )
+    )
+)
+
 
 # Agrupar por dia da semana e região e calcular a soma do tamanho
 df2 = pd.Categorical(df_2022['Dia da Semana'],categories=dias_semana_ordem, ordered=True)
@@ -67,7 +164,36 @@ df_grouped_dia_semana = df_2022.groupby(['Dia da Semana', 'Regiao'], sort=False)
 df_grouped_dia_semana.head()
 fig4 = px.line(df_grouped_dia_semana, x="Dia da Semana", y="Tamanho", color="Regiao")
 fig4.update_traces(textposition="bottom right")
-
+fig4.update_layout(
+    plot_bgcolor='rgba(0, 0, 0, 0)',  # Define a cor de fundo do gráfico como transparente
+    paper_bgcolor='#252a48',  # Define a cor de fundo do papel como '#252a48'
+    legend=dict(
+        bgcolor='rgba(255, 255, 255, 0.1)',
+        bordercolor='#252a48',
+        borderwidth=2,
+        font=dict(
+            color='white'  # Define a cor do texto da legenda como branco
+        )
+    ),
+    title_font=dict(
+        color='white'  # Define a cor do título do gráfico como branco
+    ),
+    xaxis=dict(
+        tickfont=dict(
+            color='white'  # Define a cor dos números e meses no eixo X como branco
+        ),
+        title_font=dict(
+            color='white'  # Define a cor do título do eixo X como branco
+        )
+    ),
+    yaxis=dict(
+        tickfont=dict(
+            color='white'  # Define a cor dos números no eixo Y como branco
+        ),
+        title_font=dict(
+            color='white'  # Define a cor do título do eixo Y como branco
+        )
+    ))
 df_2022.head()
 
 #Agrupando por mês e local
